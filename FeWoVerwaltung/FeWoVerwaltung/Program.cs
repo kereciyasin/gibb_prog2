68227169dce7805bd4a1007686c38fd7054924ee
Yasin Kereci <yke144907@stud.gibb.ch>
2025-06-11 13:40:41 +0200

    Neues Konsolenprojekt FeWoVerwaltung erstellt

---
+++ FeWoVerwaltung/FeWoVerwaltung/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FeWoVerwaltung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
